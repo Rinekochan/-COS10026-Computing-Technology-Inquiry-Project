d74927e60a3504950ff92eb41973c975dd0f18d8
H28J <142130945+H28J@users.noreply.github.com>
2023-10-20 19:08:28 +1100

    Update manage.php

--- manage.php
+++ manage.php
@@ -41,7 +41,6 @@
         } else {
             $currentName = "Unknown";
         }
-
         $sql_query = "";
         $sql_table = "eoi";
         $currentID = 'unactive';
@@ -53,20 +52,29 @@
         if($_SERVER["REQUEST_METHOD"] != "POST"){
             $sql_query = "SELECT * FROM $sql_table";
             $currentID = 'activeASC';
-            $_SESSION["currentSort"] = "sortID";
+            $_SESSION["currentSort"] = "sortAppID";
             $_SESSION["sortOrder"] = "ASC";
         } else {
-            if (isset($_POST["sortID"])) {
+            if (isset($_POST["listEOIsByJobRef"])) {
+                $selectedJobRef = $_POST["listByJobRef"];
+                $sql_query = "SELECT * FROM $sql_table WHERE JobRefNum = '$selectedJobRef'";
+            }
+            if (isset($_POST["listEOIsByApplicant"])) {
+                $firstName = $_POST["searchFirstName"];
+                $lastName = $_POST["searchLastName"];
+                $sql_query = "SELECT * FROM $sql_table WHERE FirstName LIKE '%$firstName%' AND LastName LIKE '%$lastName%'";
+            }
+            if (isset($_POST["sortAppID"])) {
                 // Sort by ID, toggle ascending and descending order
-                if(isset($_SESSION["currentSort"]) && $_SESSION["currentSort"] == "sortID" && $_SESSION["sortOrder"] == "ASC"){
+                if(isset($_SESSION["currentSort"]) && $_SESSION["currentSort"] == "sortAppID" && $_SESSION["sortOrder"] == "ASC"){
                     $sql_query = "SELECT * FROM $sql_table ORDER BY EOInumber DESC";
                     $currentID = 'activeDESC';
-                    $_SESSION["currentSort"] = "sortID";
+                    $_SESSION["currentSort"] = "sortAppID";
                     $_SESSION["sortOrder"] = "DESC";
                 } else {
                     $sql_query = "SELECT * FROM $sql_table ORDER BY EOInumber ASC";
                     $currentID = 'activeASC';
-                    $_SESSION["currentSort"] = "sortID";
+                    $_SESSION["currentSort"] = "sortAppID";
                     $_SESSION["sortOrder"] = "ASC";
                 }
             } elseif (isset($_POST["sortName"])) {
@@ -111,12 +119,12 @@
             } elseif (isset($_POST["sortAdd"])) {
                 // Sort by Address, toggle ascending and descending order
                 if(isset($_SESSION["currentSort"]) && $_SESSION["currentSort"] == "sortAdd" && $_SESSION["sortOrder"] == "ASC"){
-                    $sql_query = "SELECT * FROM $sql_table ORDER BY SuburbOrTown, State, PostCode DESC";
+                    $sql_query = "SELECT * FROM $sql_table ORDER BY State, PostCode DESC";
                     $currentAddress = 'activeDESC';
                     $_SESSION["currentSort"] = "sortAdd";
                     $_SESSION["sortOrder"] = "DESC";
                 } else {
-                    $sql_query = "SELECT * FROM $sql_table ORDER BY SuburbOrTown, State, PostCode ASC";
+                    $sql_query = "SELECT * FROM $sql_table ORDER BY State, PostCode ASC";
                     $currentAddress = 'activeASC';
                     $_SESSION["currentSort"] = "sortAdd";
                     $_SESSION["sortOrder"] = "ASC";
@@ -134,6 +142,16 @@
                     $_SESSION["currentSort"] = "sortStatus";
                     $_SESSION["sortOrder"] = "ASC";
                 }
+                if (isset($_POST["deleteJobRefNum"])) {
+                    $selectedJobRef = $_POST["JobRefNum"];
+                    if ($selectedJobRef !== "null") {
+                        $deleteSQL = "DELETE FROM $sql_table WHERE JobRefNum = '$selectedJobRef'";
+                        $result = mysqli_query($conn, $deleteSQL);
+                        if (!$result) {
+                            // Handle the deletion error
+                        }
+                    }
+                }                
             }
             // Add similar conditions for other sorting options
         }
@@ -156,27 +174,49 @@
                         <option value = "00100">00100</option>
                         <option value = "00101">00101</option>
                     </select>
-                    <button type = "submit" name = "deleteJobRefNum">Remove Records</button>
+                    <button type = "submit" name = "deleteJobRefNum">Remove Records</button> 
                 </div>
                 <div id = "searchQuery">
                     <label for = "search">Field</label>
-                    <select id = "search" name = "JobRefNum">
+                    <select id = "search" name = "SearchCriteria">
                         <option value = "all">All</option>
-                        <option value = "jobrefnum">Job Reference</option>
-                        <option value = "firstname">First Name</option>
-                        <option value = "lastname">Last Name</option>
-                        <option value = "fullname">Full Name</option>
-                        <option value = "status">Status</option>
+                        <option value = "JobRefNum">Job Reference</option>
+                        <option value = "FirstName">First Name</option>
+                        <option value = "LastName">Last Name</option>
+                        <option value = "FullName">Full Name</option>
+                        <option value = "Status">Status</option>
                     </select>
                     <label for = "searchquery">Search</label>
                     <input type = "text" id = "searchquery" name = "searchquery" placeholder = "Search...">
                     <button type = "submit" name = "search">Search</button>
                 </div>
             </div>
+
+            <div id="listEOIByJobRef">
+                    <label for="listByJobRef">List EOIs by Job Reference:</label>
+                    <select id="listByJobRef" name="listByJobRef">
+                        <option value="null">Choose</option>
+                        <option value="00001">Software Developer</option>
+                        <option value="00010">Network Administrator</option>
+                        <option value="00011">Cyber Security Analyst</option>
+                        <option value="00100">IT Project Manager</option>
+                        <option value="00101">Data Analyst</option>
+                    </select>
+                    <button type="submit" name="listEOIsByJobRef">List EOIs</button>
+            </div>
+
+            <div id="listEOIByApplicant">
+                    <label for="searchFirstName">Search by First Name:</label>
+                    <input type="text" id="searchFirstName" name="searchFirstName" placeholder="First Name">
+                    <label for="searchLastName">Search by Last Name:</label>
+                    <input type="text" id="searchLastName" name="searchLastName" placeholder="Last Name">
+                     <button type="submit" name="listEOIsByApplicant">Search</button>
+            </div>
+
             <table>
                 <tr id = "tableheader">
                     <?php 
-                    echo "<th><button type = 'submit' name = 'sortID' id = $currentID>ID <i class = 'fa fa-sort'></i></button></th>
+                    echo "<th><button type = 'submit' name = 'sortAppID' id = $currentID>ID <i class = 'fa fa-sort'></i></button></th>
                     <th><button type = 'submit' name = 'sortName' id = $currentApplicants>Applicants <i class = 'fa fa-sort'></i></button></th>
                     <th><button type = 'submit' name = 'sortAge' id = $currentAge>Age <i class = 'fa fa-sort'></i></button></th>
                     <th><button type = 'submit' name = 'sortJob' id = $currentJob>Preferred Job <i class = 'fa fa-sort'></i></button></th>
@@ -191,12 +231,41 @@
                         try{
                             // Attempt to connect to the database
                             $conn = mysqli_connect($host, $user, $pwd, $sql_db);
-            
+                            
                             if (!$conn){
                                 // Throw Exception if connection fail
                                 throw new Exception('Database connection error: ' . mysqli_connect_error());
                             }
-
+                            // Create eoi table if it is not existed in the database
+                            $checkTableSQL = "SHOW TABLES LIKE '$sql_table'";
+                            $result = mysqli_query($conn, $checkTableSQL);
+                            if (!$result || mysqli_num_rows($result) == 0) {
+                                $createTableSQL = "
+                                    CREATE TABLE `eoi` (
+                                        `EOInumber` int(11) NOT NULL AUTO_INCREMENT,
+                                        `JobRefNum` varchar(5) NOT NULL,
+                                        `FirstName` varchar(20) NOT NULL,
+                                        `LastName` varchar(20) NOT NULL,
+                                        `DateOfBirth` date NOT NULL,
+                                        `Gender` varchar(6) NOT NULL,
+                                        `StreetAddress` varchar(40) NOT NULL,
+                                        `SuburbOrTown` varchar(40) NOT NULL,
+                                        `State` varchar(3) NOT NULL,
+                                        `PostCode` varchar(4) NOT NULL,
+                                        `Email` varchar(50) NOT NULL,
+                                        `PhoneNumber` varchar(50) NOT NULL,
+                                        `DegreeLevel` varchar(4) NOT NULL,
+                                        `Degree` varchar(30) NOT NULL,
+                                        `Experience` varchar(30) NOT NULL,
+                                        `Skills` varchar(40) NOT NULL,
+                                        `SkillsDescription` varchar(40) NOT NULL,
+                                        `Status` varchar(20) NOT NULL,
+                                        PRIMARY KEY (`EOInumber`)
+                                    )";
+                                if(!mysqli_query($conn, $createTableSQL)){
+                                    throw new Exception('Table creation error: ' . mysqli_connect_error());
+                                }
+                            }
                             $result = mysqli_query($conn, $sql_query);
                             if (!$result) {
                                 // Redirect to an error page if there is a connection problem
@@ -220,29 +289,13 @@
                                 
                                 $dob = $row['DateOfBirth'];
                                 $age = date_diff(date_create($dob), date_create('now'))->y;
-                                
-                                $reference = $row['JobRefNum'];
-                                $jobTitle = "";
 
-                                $sql_query2 = "SELECT * FROM jobs";
-                                $result2 = mysqli_query($conn, $sql_query2);
-                                $referenceNumbers = [];
-                                $jobTitles = [];
-                                while ($row2 = mysqli_fetch_assoc($result2)) {
-                                    $ref = $row2['JobRefNum'];
-                                    $jobtitle = $row2['JobTitle'];
-                                    array_push($referenceNumbers, $ref);
-                                    array_push($jobTitles, $jobtitle);
-                                }
-                        
-                                for ($i = 0; $i < count($referenceNumbers); $i ++){
-                                    if ($reference == $referenceNumbers[$i]){
-                                        $jobTitle = $jobTitles[$i];
-                                    }
-                                }
-                                if ($jobTitle == ""){
-                                    $jobTitle = "No job in table";
-                                }
+                                $reference = $row['JobRefNum'];
+                                if($reference == "00001") $jobTitle = "Software Developer";
+                                elseif($reference == "00010") $jobTitle = "Network Administrator";
+                                elseif($reference == "00011") $jobTitle = "Cyber Security Analyst";
+                                elseif($reference == "00100") $jobTitle = "IT Project Manager";
+                                elseif($reference == "00101") $jobTitle = "Data Analyst";
 
                                 $streetAddress = $row['StreetAddress'];
                                 $addressInfo = $row['SuburbOrTown'] . ", " . $row['State'] . ", " . $row['PostCode'];
